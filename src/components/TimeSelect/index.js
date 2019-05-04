dfb44fb116df803adcb1dac7e4bd8bbcf7fe48ec
Kevin7C <cnd245006350@163.com>
2019-05-04 23:05:44 +0800

    1.对getDerivedStateFromProps的方法的修改 2.对滑动方法判断的修改
    1.对getDerivedStateFromProps的方法的修改
    2.对滑动方法判断的修改

--- src/components/TimeSelect/index.js
+++ src/components/TimeSelect/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Tooltip } from 'antd';
 import cx from 'classnames';
-import css from './index.module.scss'
+import css from './timeselect.module.scss'
 
-const TimeSelectPadding=10; //插件的左右padding
+const TimeSelectPadding = 10; //插件的左右padding
 const ValueWith = 30;  //时间块宽
 const ValueStep = 4;  //时间块 间距
 
 export default class TimeSelect extends React.PureComponent {
     static defaultProps = {
-        selectedTime: [],
         disabled: false, //是否禁用 待实现
         startTime: 8,//开始时间
         endTime: 23,//结束时间
         onChange: () => { }
+
     }
     static propTypes = {
         selectedTime: PropTypes.array,
@@ -41,17 +41,12 @@ export default class TimeSelect extends React.PureComponent {
         }
         this.state = {
             data: data,
-            selectedData: []
+            selectedData: [],
+            preSelectedTime: []
         }
         this.refTime = React.createRef();
         this.refEnd = React.createRef();
-
-        this.controlDom = {
-            lefTopX: '',
-            leftTopY: '',
-            rightBottomX: '',
-            rightBottomY: ''
-        };
+        
         this.selected = {
             type: true, // 添加 true 减少 false
             flag: false, //mouseDown 开始
@@ -66,35 +61,45 @@ export default class TimeSelect extends React.PureComponent {
         };
         this.stepList = [];// 记录每个时间块的位置
     }
-    
-    static getDerivedStateFromProps(props,state){
-        if(props.selectedTime.length){
-            for (const obj of props.selectedTime) {
-                const { data } = state;
-                let selectedIndex = data.findIndex(item => item.startTime === obj.startTime);
-                if (selectedIndex > -1) {
-                    while (true) {
-                        data[selectedIndex].selected = true;
-                        if (data[selectedIndex].endTime === obj.endTime) {
-                            break;
+
+    static getDerivedStateFromProps(nextProps, state) {
+
+        if ('selectedTime' in nextProps && nextProps.selectedTime !== undefined) {
+            if (nextProps.selectedTime === state.preSelectedTime) {
+                return state
+            }
+            const { data } = state;
+            for (let item of data) {
+                item.selected = false;
+            }
+            if (nextProps.selectedTime.length) {
+                for (const obj of nextProps.selectedTime) {
+
+                    let selectedIndex = data.findIndex(item => item.startTime === obj.startTime);
+                    if (selectedIndex > -1) {
+                        while (true) {
+                            data[selectedIndex].selected = true;
+                            if (data[selectedIndex].endTime === obj.endTime) {
+                                break;
+                            }
+                            selectedIndex++;
                         }
-                        selectedIndex++;
                     }
                 }
             }
+            return {
+                ...state,
+                preSelectedTime: nextProps.selectedTime
+            }
         }
+
         return state
     }
 
     componentDidMount() {
         const { startTime, endTime } = this.props;
-        const timeDom = this.refTime.current.getBoundingClientRect();
         const endDom = this.refEnd.current.getBoundingClientRect();
         const endX = endDom.x;
-        this.controlDom.lefTopX = timeDom.left+TimeSelectPadding;
-        this.controlDom.leftTopY = timeDom.top;
-        this.controlDom.rightBottomX = timeDom.right-TimeSelectPadding;
-        this.controlDom.rightBottomY = timeDom.bottom;
         let i = 0;
         while (i < (endTime - startTime)) {
             this.stepList.unshift({
@@ -104,7 +109,22 @@ export default class TimeSelect extends React.PureComponent {
             })
             i++
         }
+
     }
+
+    MouseUpBusiness = (index) => {
+        this.selected.flag = false;
+        const temp = JSON.parse(JSON.stringify(this.state.data));
+        const { start } = this.selected;
+        let i = Math.min(start, index);
+        const flag = this.selected.type;
+        while (i <= Math.max(start, index)) {
+            temp[i].selected = flag;
+            i++;
+        }
+        return temp
+    }
+
 
     handleMouseDown = (index, event) => {
         this.selected.flag = true;
@@ -118,46 +138,52 @@ export default class TimeSelect extends React.PureComponent {
 
     handleMouseUp = (index, event) => {
         if (this.selected.flag) {
-            this.selected.flag = false;
-            const temp = JSON.parse(JSON.stringify(this.state.data));
-            const { start } = this.selected;
-            let i = Math.min(start, index);
-            const flag = this.selected.type;
-            while (i <= Math.max(start, index)) {
-                temp[i].selected = flag;
-                i++;
+            const temp = this.MouseUpBusiness(index);
+            if (!('selectedTime' in this.props)) {
+                this.setState({
+                    data: temp
+                });
             }
-            this.setState({
-                data: temp
-            }, () => {
-                this.props.onChange(this.getSelectTime());
-            });
+            this.props.onChange(this.getSelectTime(temp));
         }
 
+    }
+
+    handleMouseUpByManual = (index) => {
+        if (this.selected.flag) {
+            const temp = this.MouseUpBusiness(index);
+            this.setState({
+                data: temp
+            });
+        }
     }
 
     handleMouseMove = (event) => {
         if (this.selected.flag) {
             const { clientX, clientY } = event;
-            const { lefTopX, leftTopY, rightBottomX, rightBottomY } = this.controlDom;
+            const timeDom = this.refTime.current.getBoundingClientRect();
+            const lefTopX = timeDom.left + TimeSelectPadding;
+            const leftTopY = timeDom.top;
+            const rightBottomX = timeDom.right - TimeSelectPadding;
+            const rightBottomY = timeDom.bottom;
+
             this.direction.beforeX = this.direction.x;
             this.direction.x = clientX;
             let flag = !this.selected.type;
-
             if (clientX <= lefTopX || clientY <= leftTopY || clientX >= rightBottomX || clientY >= rightBottomY) {
                 //判断是否出界
                 if (clientX <= lefTopX) {
-                    this.handleMouseUp(0);
+                    this.handleMouseUpByManual(0);
                     return;
                 }
                 if (clientX >= rightBottomX) {
-                    this.handleMouseUp(14);
+                    this.handleMouseUpByManual(14);
                     return;
                 }
                 const selecteItem = this.stepList.find(item => {
                     return item.start <= clientX && item.end > clientX
                 });
-                this.handleMouseUp(selecteItem.index);
+                this.handleMouseUpByManual(selecteItem.index);
                 return;
             }
 
@@ -172,20 +198,19 @@ export default class TimeSelect extends React.PureComponent {
             const selecteItem = this.stepList.find(item => {
                 return item.start <= clientX && item.end > clientX
             });
-            if(selecteItem){
+            if (selecteItem) {
                 temp[selecteItem.index].selected = flag;
                 this.setState({
                     data: temp
                 })
             }
-            
+
 
         }
     }
 
-    getSelectTime = () => {
+    getSelectTime = (data) => {
         const backArr = [];
-        const { data } = this.state;
         for (let obj of data) {
             if (obj.selected) {
                 if (backArr.length > 0) {
@@ -242,6 +267,7 @@ export default class TimeSelect extends React.PureComponent {
                             className={cx(item.selected ? css.addBlueBox : '', css.shortBox)}
                             onMouseDown={this.handleMouseDown.bind(this, index)}
                             onMouseUp={this.handleMouseUp.bind(this, index)}
+
                         >
                             <span className={css.valueText}>{item.text}</span>
                             <span className={css.valueTextLast}>{item.value + 1}</span>
@@ -255,7 +281,7 @@ export default class TimeSelect extends React.PureComponent {
                     <span key={index}
                         className={cx(item.selected ? css.addBlueBox : '', css.shortBox)}
                         onMouseDown={this.handleMouseDown.bind(this, index)}
-                        onMouseUp={this.handleMouseUp.bind(this, index)}
+                        onMouseUp={this.handleMouseUp.bind(this, index, true)}
 
                     >
                         <span className={css.valueText}>{item.text}</span>
@@ -268,10 +294,12 @@ export default class TimeSelect extends React.PureComponent {
     }
     render() {
         return (
-            <div ref={this.refTime} className={css.timeSelect} onMouseMove={this.handleMouseMove}>
-                {
-                    this.dataRender()
-                }
+            <div style={{ position: 'relative' }}>
+                <div ref={this.refTime} className={css.timeSelect} onMouseMove={this.handleMouseMove}>
+                    {
+                        this.dataRender()
+                    }
+                </div>
             </div>
         )
     }
